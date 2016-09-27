45a680bfca4128b6a9ccde9f740a41d28e2b8990
Craig Thompson <craig@flodesign.co.uk>
2016-09-27 15:33:31 +0100

    Added plugin init file

---
+++ beachfront_booking.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Flodesign Beachfront API
+Plugin URI:
+Description: API for booking management on beachfrontvillas.tc
+Version: 1.0
+Author: Craig Thompson
+Author URI: http://flodesign.co.uk
+*/
+
+require_once('Beachfront_Booking_API.php');
