a887459ae625e407f1de3b3301113196390190be
Craig Thompson <craig@flodesign.co.uk>
2016-09-29 12:57:11 +0100

    Spelling error

--- classes/Booking.php
+++ classes/Booking.php
@@ -260,7 +260,7 @@ class Booking
         
         $booking = get_posts($args);
         
-        if(count($bookings > 0)){
+        if(count($booking > 0)){
             return wp_delete_post($booking[0]->ID);
         } else {
             return new Exception('Could not find a booking with that ID');
